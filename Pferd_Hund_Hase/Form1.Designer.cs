63a68c9e4c3c43db088229bae21058e90bcd7b9c
SSinghSahib <terayog@web.de>
2023-12-29 14:53:50 +0100

    Projektdateien hinzufügen.

---
+++ Pferd_Hund_Hase/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Pferd_Hund_Hase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Timers.Timer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1137, 21);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1137, 21);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1137, 21);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, 503);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1137, 21);
+            this.label4.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(1135, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 524);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "F\r\nI\r\nN\r\nI\r\nS\r\nH";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(12, 543);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 61);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(606, 543);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(531, 61);
+            this.panel1.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(67, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(430, 32);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "label6";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 133);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(4, 192);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(152, 133);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 15;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(9, 361);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(152, 133);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1180, 629);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Pferd_Hund_Hase_Spiel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Timers.Timer timer1;
+
+        private System.Windows.Forms.Label label6;
+
+        private System.Windows.Forms.Panel panel1;
+
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
+
---
+++ Pferd_Hund_Hase/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms; 
+
+namespace Pferd_Hund_Hase
+{
+    public partial class Form1 : Form
+    {
+        int LeftHund, LeftHase, LeftPferd;
+        public bool start;
+
+        //Zufallsgenerator
+        Random rnd = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LeftHund = pictureBox1.Left;
+            LeftHase = pictureBox2.Left;
+            LeftPferd = pictureBox3.Left;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Stop();
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox3.Enabled = false;
+        }        
+         private void button1_Click(object sender, EventArgs e)
+         {
+             timer1.Enabled = false;
+             start = true;
+             timer1.Start();
+             pictureBox1.Enabled = true;
+             pictureBox2.Enabled = true;
+             pictureBox3.Enabled = true;
+         }
+
+         private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+         {
+             int widthHund = pictureBox1.Width;
+             int widthHase = pictureBox2.Width;
+             int widthPferd = pictureBox3.Width;
+             int start = label5.Left;
+             pictureBox1.Left = pictureBox1.Left + rnd.Next(5, 16);
+             pictureBox2.Left = pictureBox2.Left + rnd.Next(5, 16);
+             pictureBox3.Left = pictureBox3.Left + rnd.Next(5, 16);
+             
+             if (pictureBox1.Left>pictureBox2.Left+5 && pictureBox1.Left>pictureBox3.Left+5)
+             {
+                 label6.Text = "Der Hund führt";
+             }
+             if (pictureBox2.Left>pictureBox1.Left+5 && pictureBox2.Left>pictureBox3.Left+5)
+             {
+                 label6.Text = "Der Hase führt";
+             }
+             if (pictureBox3.Left>pictureBox1.Left+5 && pictureBox3.Left>pictureBox2.Left+5)
+             {
+                 label6.Text = "Das Pferd führt";
+             }
+            //Wenn der Hund gewinnt
+            if (widthHund+pictureBox1.Left>=start)
+             {
+                 timer1.Enabled = false;
+                 label6.Text = "Der Hund hat gewonnen";
+                 pictureBox1.Enabled = false;
+                 pictureBox2.Enabled = false;
+                 pictureBox3.Enabled = false;
+            }
+            //Wenn der Hase gewinnt
+            if (widthHase+pictureBox2.Left>=start)
+             {
+                 timer1.Enabled = false;
+                 label6.Text = "Der Hase hat gewonnen";
+                 pictureBox1.Enabled = false;
+                 pictureBox2.Enabled = false;
+                 pictureBox3.Enabled = false;
+            }
+            //Wenn das Pferd gewinnt
+            if (widthPferd+pictureBox3.Left>=start)
+             {
+                 timer1.Enabled = false;
+                 label6.Text = "Das Pferd hat gewonnen";
+                 pictureBox1.Enabled = false;
+                 pictureBox2.Enabled = false;
+                 pictureBox3.Enabled = false;
+            }         
+         }      
+    }
+}
